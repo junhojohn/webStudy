a04df6f1f0da423d1c79e497e5bc7c21fb22bff4
junhojohn <junhojohn@gmail.com>
2020-03-16 20:40:42 +0900

    example of Spring DI + MyBatis

---
+++ SpringWebProject05/src/main/java/ibatis/services/user/test/MyBatisTestApp12.java
@@ -0,0 +1,76 @@
+package ibatis.services.user.test;
+
+import java.util.ArrayList;
+
+import mybatis.service.daos.UserDaoImpl;
+import mybatis.service.domain.Search;
+import mybatis.service.domain.User;
+import mybatis.service.services.UserServiceImpl;
+
+import org.apache.ibatis.session.SqlSession;
+
+/*
+ * FileName : MyBatisTestApp11.java 
+ *  :: Persistence Layer unit Test : MyBatis + DAO
+  */
+public class MyBatisTestApp12 {
+	
+	///main method
+	public static void main(String[] args) throws Exception{
+
+		//==> SqlSessionFactoryBean.getSqlSession()을 이용 SqlSession instance GET
+		SqlSession sqlSession = SqlSessionFactoryBean.getSqlSession();
+		
+		//==> UserDaoImpl11 생성 및 sqlSession instance setter injection
+		UserDaoImpl userDao = new UserDaoImpl();
+		userDao.setSqlSession(sqlSession);
+		System.out.println("\n");
+		
+		UserServiceImpl userService = new UserServiceImpl();
+		userService.setUserDao(userDao);
+		
+		//==> Test 용 User instance 생성  
+		User user = new User("user04","주몽","user04",3,0);
+		
+		//1. addUser Test  
+		System.out.println(":: 1. addUser(INSERT)  ? ");
+		System.out.println(":: "+ userService.addUser(user) );
+		System.out.println("\n");
+		
+		//2. getUser Test :: 주몽 inert 확인 
+		System.out.println(":: 2. getUser(SELECT)  ? ");
+		System.out.println(":: "+ userService.getUser(user.getId()) );
+		System.out.println("\n");
+
+		//3. uadateUser Test  :: 주몽 ==> 온달 수정
+		user.setId("온달");
+		System.out.println(":: 3. update(UPDATE)  ? ");
+		System.out.println(":: "+userService.modifyUser(user) );
+		System.out.println("\n");
+		
+		//4. getUserList Test ::
+		System.out.println(":: 4. getUserList(SELECT)  ? ");
+		Search search = new Search();
+		search.setSearchCondition("userId");
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add("user04");
+		search.setIdList( arrayList );
+		
+		System.out.println(":: List<User> 내용 : "+ userService.getAllUserList(search) );
+		System.out.println("\n");
+		
+		//5. removeUser Test
+		System.out.println(":: 5. removeUser (DELETE)  ? ");
+		System.out.println(":: "+userService.removeUser(user.getId()) );
+		System.out.println("\n");
+		
+		//6. getUserList Test 
+		System.out.println(":: 6. getUserList(SELECT)  ? ");
+		System.out.println("List<User> 내용 : "+ userService.getAllUserList(search) );
+		System.out.println("\n");
+		
+		//END::SqlSession  close
+		System.out.println("::END::SqlSession 닫기..");
+		sqlSession.close();
+	}//end of main
+}//end of class
