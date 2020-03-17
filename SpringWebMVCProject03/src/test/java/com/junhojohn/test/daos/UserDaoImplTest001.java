c4d6c4afcb73662a901267ff18a50c6ba3b2d9e8
junhojohn <junhojohn@gmail.com>
2020-03-18 00:25:49 +0900

    implementing test method

---
+++ SpringWebMVCProject03/src/test/java/com/junhojohn/test/daos/UserDaoImplTest001.java
@@ -0,0 +1,23 @@
+package com.junhojohn.test.daos;
+
+import junit.framework.TestCase;
+
+public class UserDaoImplTest001 extends TestCase {
+
+	public void testUserDaoImpl() {
+		fail("Not yet implemented");
+	}
+
+	public void testUserDaoImplSqlSession() {
+		fail("Not yet implemented");
+	}
+
+	public void testSelectUserVO() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetSqlSession() {
+		fail("Not yet implemented");
+	}
+
+}
